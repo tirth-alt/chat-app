64885e9a59beb071ff68cac427f01b223ebf0e7a
tirth-alt <tirths1926@gmail.com>
2025-10-25 22:23:27 +0530

    Deploy backend

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
